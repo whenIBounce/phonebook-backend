7d4dcbcb194c1ccf3c30e4b3f51d79c22d20f008
= <zhan4868@umn.edu>
2022-07-16 13:16:19 +0800

    exercise 3.6

--- index.js
+++ index.js
@@ -66,10 +66,15 @@ app.delete('/api/persons/:id', (req, res) => {
 
 app.post('/api/persons', (req, res) => {
   const body = req.body
+  const nameExisted = persons.find((p) => p.name === body.name)
 
   if (!body.name || !body.number) {
     return res.status(400).json({
       error: 'name or number missing',
+    })
+  } else if (nameExisted) {
+    return res.status(400).json({
+      error: 'name must be unique',
     })
   } else {
     const newPersonObject = {
