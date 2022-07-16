d0403d75267d1d9ec48395a09a277f339345b794
= <zhan4868@umn.edu>
2022-07-16 12:20:08 +0800

    Initial project version

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+/* The json-parser functions 
+so that it takes the JSON data of a request, 
+transforms it into a JavaScript object 
+and then attaches it 
+to the body property of the request object
+before the route handler is called */
+
+let persons = [
+  {
+    id: 1,
+    name: 'Arto Hellas',
+    number: '040-123456',
+  },
+  {
+    id: 2,
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+  },
+  {
+    id: 3,
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+  },
+  {
+    id: 4,
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+  },
+]
+
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+  /* Calling the method will send the notes array 
+  that was passed to it as a JSON formatted string. 
+  Express automatically sets the Content-Type header 
+  with the appropriate value of application/json. */
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
