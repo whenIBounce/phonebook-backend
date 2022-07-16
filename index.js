1052010fd6414e4ecd823752962b72f77ba29ce2
= <zhan4868@umn.edu>
2022-07-16 12:58:17 +0800

    exercise 3.4

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { application } = require('express')
 const express = require('express')
 const app = express()
 
@@ -55,6 +56,12 @@ app.get('/api/persons/:id', (req, res) => {
   } else {
     res.status(404).end()
   }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter((p) => p.id !== id)
+  res.status(204).end()
 })
 
 const PORT = 3001
